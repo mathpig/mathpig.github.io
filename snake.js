4baa78aa7d1fffc3ab61b207e6b3eca6cedc6f9d
mathpig <41877708+mathpig@users.noreply.github.com>
2021-01-04 14:12:03 -0800

    Added snake game.

---
+++ snake.js
@@ -0,0 +1,108 @@
+'use strict';
+
+const WIDTH = 30;
+const HEIGHT = 15;
+
+var screen = document.getElementById('screen');
+var ctx = screen.getContext('2d');
+
+var score = 0;
+var length;
+var eggX, eggY;
+var snake = [];
+var directionX, directionY;
+
+function Reset() {
+  score = 0;
+  directionX = 0;
+  directionY = 0;
+  length = 3;
+  snake = [[Math.floor(WIDTH / 2), Math.floor(HEIGHT / 2)]];
+  NewEgg();
+}
+
+function NewEgg() {
+  eggX = Math.floor(Math.random() * WIDTH);
+  eggY = Math.floor(Math.random() * HEIGHT);
+}
+
+function Draw() {
+  screen.width = window.innerWidth;
+  screen.height = window.innerHeight;
+
+  ctx.fillStyle = '#040';
+  ctx.fillRect(0, 0, screen.width, screen.height);
+
+  ctx.save();
+
+  ctx.translate(0, 50);
+  ctx.scale(screen.width / WIDTH, (screen.height - 50) / HEIGHT);
+
+  ctx.fillStyle = '#030';  
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+  for (var i = 0; i < snake.length; ++i) {
+    if (i == snake.length - 1) {
+      ctx.fillStyle = '#ff0';
+    }
+    else {
+      ctx.fillStyle = '#f00';
+    }
+    ctx.fillRect(snake[i][0], snake[i][1], 1, 1);
+  }
+  
+  var egg1 = document.getElementById('egg1');
+  ctx.drawImage(egg1, eggX, eggY, 1, 1);
+
+  ctx.restore();
+  
+  ctx.fillStyle = '#fff';
+  ctx.font = '40px san-serif';
+  ctx.fillText('Score: ' + score, 50, 37.5)
+}
+
+function Tick() {
+  var head = snake[snake.length - 1]; 
+  if (head[0] == eggX && head[1] == eggY) {
+    score += length;
+    length++;
+    NewEgg();
+  }
+  if (head[0] < 0 || head[0] >= WIDTH ||
+      head[1] < 0 || head[1] >= HEIGHT) {
+    Reset();
+    return;
+  }
+  for (var i = 0; i < snake.length - 1; ++i) {
+    if (head[0] == snake[i][0] && head[1] == snake[i][1]) {
+      Reset();
+      return;
+    }
+  }
+  if (directionX != 0 || directionY != 0) {
+    snake.push([head[0] + directionX, head[1] + directionY]);
+  }
+  if (snake.length > length) {
+    snake.splice(0, 1);
+  }
+  Draw();
+}
+
+Reset();
+setInterval(Tick, 200);
+
+window.onkeydown = function(e) {
+  if (e.code == 'ArrowLeft') {
+    directionX = -1;
+    directionY = 0;
+  } else if (e.code == 'ArrowRight') {
+    directionX = 1;
+    directionY = 0;
+  } else if (e.code == 'ArrowUp') {
+    directionX = 0;
+    directionY = -1;
+  } else if (e.code == 'ArrowDown') {
+    directionX = 0;
+    directionY = 1;
+  }
+};
