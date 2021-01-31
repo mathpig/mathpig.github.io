7187ee6beeef991dd114e68c6606b25ba2bbfe7b
mathpig <41877708+mathpig@users.noreply.github.com>
2021-01-30 19:55:21 -0800

    Added several screens.

--- pigmurder.js
+++ pigmurder.js
@@ -1,5 +1,41 @@
 'use strict';
 
+var mainScreen = document.getElementById('main_screen');
+var settingsScreen = document.getElementById('settings_screen');
+var createGameScreen = document.getElementById('create_game_screen');
+var findGameScreen = document.getElementById('find_game_screen');
+var enterCodeScreen = document.getElementById('enter_code_screen');
+
+document.getElementById('create_game_button').onclick = function() {
+  mainScreen.style.display = 'none';
+  createGameScreen.style.display = '';
+};
+
+document.getElementById('create_game_back').onclick = function() {
+  createGameScreen.style.display = 'none';
+  mainScreen.style.display = '';
+};
+
+document.getElementById('find_game_button').onclick = function() {
+  mainScreen.style.display = 'none';
+  findGameScreen.style.display = '';
+};
+
+document.getElementById('find_game_back').onclick = function() {
+  findGameScreen.style.display = 'none';
+  mainScreen.style.display = '';
+};
+
+document.getElementById('enter_code_button').onclick = function() {
+  mainScreen.style.display = 'none';
+  enterCodeScreen.style.display = '';
+};
+
+document.getElementById('enter_code_back').onclick = function() {
+  enterCodeScreen.style.display = 'none';
+  mainScreen.style.display = '';
+};
+
 var nameBox = document.getElementById('nameBox');
 
 var firebaseConfig = {
