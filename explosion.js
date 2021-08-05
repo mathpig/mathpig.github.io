484ef0d5b2c022f638249f146e249e7ac2e391bd
mathpig <41877708+mathpig@users.noreply.github.com>
2021-08-05 12:04:06 -0700

    Did stuff.

--- explosion.js
+++ explosion.js
@@ -2,6 +2,7 @@
 
 //const NUM_BLOCKS = 5;
 //const BLOCKS = [AIR, GRASS, ROCK, LAVA, BEDROCK];
+//const REGULARNESS = 10;
 
 function Distance(x1, y1, z1, x2, y2, z2) {
   var dx = x1 - x2;
@@ -11,8 +12,8 @@ function Distance(x1, y1, z1, x2, y2, z2) {
 }
 
 function Explode(chunk_set, tx, ty, tz) {
-  var blast_radius = Math.floor(Math.random() * 6) +
-                     Math.floor(Math.random() * 6);
+  var blast_radius = Math.floor(Math.random() * 26) +
+                     Math.floor(Math.random() * 26);
   for (var x = tx - blast_radius; x <= tx + blast_radius; ++x) {    
     for (var y = ty - blast_radius; y <= ty + blast_radius; ++y) {
       for (var z = tz - blast_radius; z <= tz + blast_radius; ++z) {
@@ -22,8 +23,8 @@ function Explode(chunk_set, tx, ty, tz) {
         if (Distance(x, y, z, tx, ty, tz) <= blast_radius &&
             Math.random() * 3 <= 2 ||
             Distance(x, y, z, tx, ty, tz) <= Math.floor(blast_radius / 2)) {
-          chunk_set.change(x, y, z, AIR);
-//          chunk_set.change(x, y, z, BLOCKS[Math.floor(Math.random() * NUM_BLOCKS)]);
+          chunk_set.change(x, y, z, ROCK);
+//          chunk_set.change(x, y, z, BLOCKS[Math.floor(Math.pow(Math.random(), REGULARNESS) * NUM_BLOCKS)]);
         }
       }
     }
