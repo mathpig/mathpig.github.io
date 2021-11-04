60b7ffa9b918fc21b49511fc0fee4d64ae7ed90b
mathpig <41877708+mathpig@users.noreply.github.com>
2021-11-04 10:11:59 -0700

    Fixed some of the bugs by breaking into functions. Some still remain.

--- tit-for-tat_special.js
+++ tit-for-tat_special.js
@@ -8,114 +8,52 @@ var turns_locked = false;
 var recent_choices = [];
 var tit_for_tat_on_permanently = false;
 var tit_for_tat_on_temporarily = false;
-var tit_for_tat_count = 0;
+var tit_for_tat_on_count = 0;
 var on_probation = false;
 var on_probation_count = 0;
 var computers_choice = 'P';
 
-function DoRound(your_choice, computers_choice) {
-  if (!turns_locked) {
-    turns_locked = true;
-    turns_left = parseInt(turns.value);
-    turns.disabled = true;
-  }
-  if (turns_left <= 0) {
-    passive.onclick = null;
-    aggressive.onclick = null;
-    return;
-  }
+function Terminate() {
+  passive.onclick = null;
+  agressive.onclick = null;
+}
 
-  computers_choice = 'P';
-
-  if (recent_choices.length == 1) {
-    if (recent_choices[0] == 'A') {
-      tit_for_tat_on_temporarily = true;
-      tit_for_tat_count = 20;
+function TestForShortMediumAndLongTermOffences(short_term_choices, short_term_unacceptable_aggressive_val, medium_term_choices, medium_term_unacceptable_aggressive_val, long_term_choices, long_term_unacceptable_aggressive_val) {
+  var aggressive_count = 0;
+  for (var i = 0; i < short_term_choices.length; ++i) {
+    if (short_term_choices[i] == 'A') {
+      aggressive_count++;
     }
   }
-
-  var short_term_recent_choices = recent_choices.slice(Math.max(recent_choices.length - 4, 0), recent_choices.length);
-  var medium_term_recent_choices = recent_choices.slice(Math.max(recent_choices.length - 21, 0), recent_choices.length);
-  var long_term_recent_choices = recent_choices;
-
-  if (on_probation == false && tit_for_tat_on_temporarily == false && tit_for_tat_on_permanently == false) {
-    var aggressive_count = 0;
-    for (var i = 0; i < short_term_recent_choices.length; ++i) {
-      if (short_term_recent_choices[i] == 'A') {
-        aggressive_count++;
-      }
-    }
-    if (aggressive_count >= 2) {
-      tit_for_tat_on_temporarily = true;
-      tit_for_tat_count = 20;
-    }
-
-    var aggressive_count = 0;
-    for (var i = 0; i < medium_term_recent_choices.length; ++i) {
-      if (short_term_recent_choices[i] == 'A') {
-        aggressive_count++;
-      }
-    }
-    if (aggressive_count >= 3) {
-      tit_for_tat_on_temporarily = true;
-      tit_for_tat_count = 20;
-    }
-
-    var aggressive_count = 0;
-    for (var i = 0; i < long_term_recent_choices.length; ++i) {
-      if (short_term_recent_choices[i] == 'A') {
-        aggressive_count++;
-      }
-    }
-    if (aggressive_count >= 5) {
-      tit_for_tat_on_temporarily = true;
-      tit_for_tat_count = 20;
-    }
+  if (aggressive_count >= short_term_unacceptable_aggressive_val) {
+    tit_for_tat_on_temporarily = true;
+    tit_for_tat_on_count = 20;
   }
 
-  if (on_probation == false && tit_for_tat_on_temporarily && tit_for_tat_on_permanently == false && tit_for_tat_count > 0) {
-    computers_choice = recent_choices[recent_choices.length - 1];
-    tit_for_tat_count--;
-  }
-
-  if (tit_for_tat_on_temporarily && tit_for_tat_count == 0) {
-    on_probation = true;
-    on_probation_count = 50;
-    tit_for_tat_on_temporarily = false;
-  }
-  if (tit_for_tat_on_permanently == false && on_probation && on_probation_count > 0) {
-    var aggressive_count = 0;
-    for (var i = 0; i < medium_term_recent_choices.length; ++i) {
-      if (short_term_recent_choices[i] == 'A') {
-        aggressive_count++;
-      }
+  var aggressive_count = 0;
+  for (var i = 0; i < medium_term_choices.length; ++i) {
+    if (medium_term_choices[i] == 'A') {
+      aggressive_count++;
     }
-    if (aggressive_count >= 2) {
-      tit_for_tat_on_permanently = true;
-    }
-
-    var aggressive_count = 0;
-    for (var i = 0; i < long_term_recent_choices.length; ++i) {
-      if (short_term_recent_choices[i] == 'A') {
-        aggressive_count++;
-      }
-    }
-    if (aggressive_count >= 4) {
-      tit_for_tat_on_permanently = true;
-    }
-    on_probation_count--;
+  }
+  if (aggressive_count >= medium_term_unacceptable_aggressive_val) {
+    tit_for_tat_on_temporarily = true;
+    tit_for_tat_on_count = 20;
   }
 
-  if (tit_for_tat_on_permanently) {
-    computers_choice = recent_choices[recent_choices.length - 1];
+  var aggressive_count = 0;
+  for (var i = 0; i < long_term_choices.length; ++i) {
+    if (long_term_choices[i] == 'A') {
+      aggressive_count++;
+    }
   }
-
-  if (recent_choices.length >= 100) {
-    recent_choices.shift();
+  if (aggressive_count >= long_term_unacceptable_aggressive_val) {
+    tit_for_tat_on_temporarily = true;
+    tit_for_tat_on_count = 20;
   }
+}
 
-  recent_choices.push(your_choice);
-
+function DollOutMoney(your_choice, computers_choice) {
   if (your_choice == 'P' && computers_choice == 'P') {
     your_money += 75;
     computers_money += 75;
@@ -132,6 +70,73 @@ function DoRound(your_choice, computers_choice) {
     your_money += 50;
     computers_money += 50;
   }
+}
+
+function DoRound(your_choice, computers_choice) {
+  if (!turns_locked) {
+    turns_locked = true;
+    turns_left = parseInt(turns.value);
+    turns.disabled = true;
+  }
+  if (turns_left <= 0) {
+    Terminate();
+    return;
+  }
+
+  computers_choice = 'P';
+
+  if (recent_choices.length == 1) {
+    if (recent_choices[0] == 'A') {
+      tit_for_tat_on_temporarily = true;
+      tit_for_tat_on_count = 20;
+    }
+  }
+
+  var short_term_choices = recent_choices.slice(Math.max(recent_choices.length - 4, 0), recent_choices.length);
+  var medium_term_choices = recent_choices.slice(Math.max(recent_choices.length - 21, 0), recent_choices.length);
+  var long_term_choices = recent_choices;
+
+  if (on_probation == false && tit_for_tat_on_temporarily == false && tit_for_tat_on_permanently == false) {
+    TestForShortMediumAndLongTermOffences(short_term_choices, 2, medium_term_choices, 3, long_term_choices, 5);
+  }
+
+  if (on_probation == false && tit_for_tat_on_temporarily && tit_for_tat_on_permanently == false && tit_for_tat_on_count > 0) {
+    computers_choice = recent_choices[recent_choices.length - 1];
+    tit_for_tat_on_count--;
+  }
+
+  if (tit_for_tat_on_temporarily && tit_for_tat_on_count <= 0) {
+    on_probation = true;
+    on_probation_count = 50;
+    tit_for_tat_on_temporarily = false;
+  }
+  if (tit_for_tat_on_permanently == false && on_probation && on_probation_count > 0) {
+    TestForShortMediumAndLongTermOffences(short_term_choices, 2, medium_term_choices, 2, long_term_choices, 4);
+    if (tit_for_tat_on_temporarily) {
+      tit_for_tat_on_permanently = true;
+      tit_for_tat_on_temporarily = false;
+      tit_for_tat_on_count = 0;
+      on_probation = false;
+      on_probation_count = 0;
+    }
+  }
+
+  if (on_probation && on_probation_count <= 0) {
+    on_probation = false;
+  }
+
+  if (tit_for_tat_on_permanently) {
+    computers_choice = recent_choices[recent_choices.length - 1];
+  }
+
+  if (recent_choices.length >= 100) {
+    recent_choices.shift();
+  }
+
+  recent_choices.push(your_choice);
+
+  DollOutMoney(your_choice, computers_choice);
+
   turns_left--;
   turns.value = turns_left;
   yourmoney.innerText = your_money;
