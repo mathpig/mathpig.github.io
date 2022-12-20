591fabe47559db812b813fc02dbd0af1c3dd7880
mathpig <41877708+mathpig@users.noreply.github.com>
2022-12-20 12:18:28 -0800

    Added support for multiple walls in ink

--- textGames/ink.js
+++ textGames/ink.js
@@ -1,37 +1,112 @@
-'use strict';
-
 async function main() {
-  var size = randint(25, 35);
-  var maxValue = randint(5, 7);
-
-  var startX = randint(0, size - 1);
-  var startY = randint(0, size - 1);
+  var gameMap = [];
 
   var lastColor = 0;
 
-  if (maxValue == 7) {
-    var movesRemaining = randint(Math.round(3 * size / 2), 2 * size);
-  }
-  else {
-    var movesRemaining = randint(Math.round(3 * size / 2), Math.round(7 * size / 4));
+  var movesRemaining = 0;
+  var size = 0;
+  var maxValue = 0;
+  var startX = 0;
+  var startY = 0;
+
+  function generateMap() {
+    size = randint(25, 35);
+    maxValue = randint(5, 7);
+
+    startX = randint(0, size - 1);
+    startY = randint(0, size - 1);
+
+    if (maxValue == 7) {
+      movesRemaining = randint(Math.round(3 * size / 2), 2 * size);
+    }
+    else {
+      movesRemaining = randint(Math.round(3 * size / 2), Math.round(7 * size / 4));
+    }
+
+    gameMap = [];
+    for (var i = 0; i < size; ++i) {
+      gameMap.push([]);
+      for (var j = 0; j < size; ++j) {
+        gameMap[i].push(randint(1, maxValue));
+      }
+    }
+
+    for (var i = 0; i < randint(40, 60); ++i) {
+      gameMap[randint(0, size - 1)][randint(0, size - 1)] = "W";
+    }
+
+    gameMap[startX][startY] = "X";
   }
 
-  var gameMap = [];
-
-  for (var i = 0; i < size; ++i) {
-    gameMap.push([]);
-    for (var j = 0; j < size; ++j) {
-      gameMap[i].push(randint(1, maxValue));
+  function checkMap() {
+    for (var i = 0; i < size; ++i) {
+      for (var j = 0; j < size; ++j) {
+        if (gameMap[i][j] != "W" && gameMap[i][j] != "X") {
+          return specialFloodFill(i, j);
+        }
+      }
     }
   }
 
-  for (var i = 0; i < randint(0, 1); ++i) {
-    gameMap[randint(0, size - 1)][randint(0, size - 1)] = "W";
+  function copyMap() {
+    var copiedMap = [];
+    for (var i = 0; i < size; ++i) {
+      copiedMap.push([]);
+      for (var j = 0; j < size; ++j) {
+        copiedMap[i].push(gameMap[i][j]);
+      }
+    }
+    return copiedMap;
   }
 
-  gameMap[startX][startY] = "X";
+  function specialFloodFill(x, y) {
+    copiedMap = copyMap();
 
-  function printMap() {
+    toDo = [[x, y]];
+    while (toDo.length > 0) {
+      var n = toDo.pop();
+      var x = n[0];
+      var y = n[1];
+
+      if (copiedMap[x][y] == "X" || copiedMap[x][y] == "W" || copiedMap[x][y] == "-") {
+        continue;
+      }
+
+      copiedMap[x][y] = "-";
+
+      if (x > 0) {
+        toDo.push([x - 1, y]);
+      }
+      if (y > 0) {
+        toDo.push([x, y - 1]);
+      }
+      if (x < (size - 1)) {
+        toDo.push([x + 1, y]);
+      }
+      if (y < (size - 1)) {
+        toDo.push([x, y + 1]);
+      }
+    }
+
+    for (var i = 0; i < size; ++i) {
+      for (var j = 0; j < size; ++j) {
+        if (copiedMap[i][j] != "X" && copiedMap[i][j] != "W" && copiedMap[i][j] != "-") {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  function generateGoodMap() {
+    generateMap();
+    while (!checkMap()) {
+      generateMap();
+    }
+  }
+
+  function printMap(m) {
     if (movesRemaining == 1) {
       print("You have 1 move left.");
     }
@@ -41,11 +116,11 @@ async function main() {
     print("");
     for (var i = 0; i < size; ++i) {
       for (var j = 0; j < size; ++j) {
-        if (gameMap[i][j] != "W" && gameMap[i][j] != "X") {
+        if (m[i][j] != "W" && m[i][j] != "X" && m[i][j] != "-") {
           foreground(0);
-          background(gameMap[i][j]);
+          background(m[i][j]);
         }
-        print(gameMap[i][j] + " ", "");
+        print(m[i][j] + " ", "");
         normal();
       }
       print("");
@@ -53,12 +128,12 @@ async function main() {
   }
 
   function floodFill(x, y, color1, color2) {
-    var toDo = [[x, y]];
+    toDo = [[x, y]];
 
     while (toDo.length > 0) {
-      var arr = toDo.pop();
-      var x = arr[0];
-      var y = arr[1];
+      var n = toDo.pop()
+      var x = n[0];
+      var y = n[1];
 
       if (gameMap[x][y] != color1) {
         continue;
@@ -67,16 +142,16 @@ async function main() {
       gameMap[x][y] = color2;
 
       if (x > 0) {
-        toDo.push([x - 1, y])
+        toDo.push([x - 1, y]);
       }
       if (y > 0) {
-        toDo.push([x, y - 1])
+        toDo.push([x, y - 1]);
       }
       if (x < (size - 1)) {
-        toDo.push([x + 1, y])
+        toDo.push([x + 1, y]);
       }
       if (y < (size - 1)) {
-        toDo.push([x, y + 1])
+        toDo.push([x, y + 1]);
       }
     }
   }
@@ -86,29 +161,30 @@ async function main() {
     for (var i = 0; i < size; ++i) {
       for (var j = 0; j < size; ++j) {
         if (gameMap[i][j] != lastColor && gameMap[i][j] != "W" && gameMap[i][j] != "X") {
-          count += 1;
+          count++;
         }
       }
     }
     return count;
   }
 
+  generateGoodMap();
   print("");
   if (maxValue == 5) {
     print("This is an easy puzzle.");
   }
   else if (maxValue == 6) {
-    print("This is a medium puzzle.");
+    print("This is a medium puzzle.")
   }
   else {
-    print("This is a hard puzzle.");
+    print("This is a hard puzzle, maybe even impossible.")
   }
   print("");
-  printMap();
+  printMap(gameMap);
   print("");
 
   while (movesRemaining > 0) {
-    lastColor = await get_int("Enter a color to fill with: ");
+    var lastColor = await get_int("Enter a color to fill with: ");
     while (lastColor <= 0 || lastColor > maxValue) {
       lastColor = await get_int("Enter a color to fill with (has to be between 1 and " + maxValue + ", inclusive): ");
     }
@@ -128,7 +204,7 @@ async function main() {
 
     movesRemaining--;
     print("");
-    printMap();
+    printMap(gameMap);
     print("");
 
     if (hasWon(lastColor) == 0) {
