10dd157edba818a998c140bae81766813b0d69d3
mathpig <41877708+mathpig@users.noreply.github.com>
2022-12-20 22:29:45 -0800

    Added fix to prevent slow printing.

--- textGames/textConsole.js
+++ textGames/textConsole.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const MAX_LINES = 400;
+
 inprompt.focus();
 document.body.style.backgroundColor = 'black';
 
@@ -16,7 +18,25 @@ const COLORS = [
 
 var fg_color, bg_color;
 var pending = document.createElement('span');
+var pending_lines = 0;
+var output_lines = 0;
+var added = [];
 normal();
+
+function countChar(s, ch) {
+  var n = 0;
+  var pos = 0;
+  for (;;) {
+    var i = s.indexOf(ch, pos);
+    if (i >= 0) {
+      pos = i + 1;
+      n++;
+    } else {
+      break;
+    }
+  }
+  return n;
+}
 
 function print(s, end) {
   if (end === undefined) {
@@ -27,6 +47,7 @@ function print(s, end) {
   span.style.backgroundColor = bg_color;
   span.innerText = s + end;
   pending.appendChild(span);
+  pending_lines += countChar(s + end, '\n');
 }
 
 function foreground(c) {
@@ -43,8 +64,16 @@ function normal() {
 }
 
 function flush() {
+  added.push([pending, pending_lines]);
+  output_lines += pending_lines;
   output.appendChild(pending);
   pending = document.createElement('span');
+  pending_lines = 0;
+  while (added.length > 1 && output_lines > MAX_LINES) {
+    output.removeChild(added[0][0]);
+    output_lines -= added[0][1];
+    added.splice(0, 1);
+  }
   window.scrollTo(0, document.body.clientHeight);
 }
 
@@ -114,4 +143,11 @@ function shuffle(arr) {
 function exit() {
   flush();
   return new Promise(function(resolve, reject) {});
+}
+
+function clear() {
+  for (var i = 0; i < added.length; i++) {
+    output.removeChild(added[i]);
+  }
+  added = [];
 }
