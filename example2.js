63c227b039b14be46986fb2653b8646fa5c94415
mathpig <41877708+mathpig@users.noreply.github.com>
2023-06-08 14:45:56 -0700

    Stuff.

--- example2.js
+++ example2.js
@@ -3,46 +3,56 @@
 var screen = document.getElementById("screen");
 var ctx = screen.getContext("2d");
 
-var image = ctx.createImageData(screen.width, screen.height);
-
 var arr = [];
+var d = {};
 
-function Draw() {
-  var data = image.data;
-  var pos = 0;
+var barSize = 1;
+
+var start = 0;
+var hasDone = false;
+
+for (var i = 0; i < Math.floor(screen.width / barSize); ++i) {
+  do {
+    var val = Math.floor(Math.random() * screen.width / barSize) + 1;
+  } while (val in d);
+  arr.push(val);
+  d[val] = true;
+}
+
+function Draw(highlightIndex) {
+  ctx.fillStyle = "red";
+  ctx.fillRect(0, 0, screen.width, screen.height);
   for (var i = 0; i < screen.width; ++i) {
-    for (var j = 0; j < screen.height; ++j) {
-      if (arr[i] >= (screen.height - j)) {
-        var r = 0;
-        var g = 255;
-      }
-      else {
-        var r = 255;
-        var g = 0;
-      }
-      var b = 0;
-      data[pos++] = r;
-      data[pos++] = g;
-      data[pos++] = b;
-      data[pos++] = 255;
+    ctx.fillStyle = "green";
+    if (i == highlightIndex || i == (highlightIndex + 1)) {
+      ctx.fillStyle = "yellow";
     }
+    ctx.fillRect(i * barSize, screen.height - barSize * arr[i], barSize, barSize * arr[i]);
   }
 }
 
 function Tick() {
-  for (var i = 0; i < (arr.length - 1); ++i) {
+  if (!hasDone) {
+    hasDone = true;
+    Draw(-2);
+    return;
+  }
+  hasDone = false;
+  for (var i = start; i < (arr.length - 1); ++i) {
     if (arr[i] > arr[i + 1]) {
       var tmp = arr[i];
       arr[i] = arr[i + 1];
       arr[i + 1] = tmp;
-      await new Promise(r => setTimeout(r, 50));
-      Draw();
+      Draw(i);
+      start = (i + 1);
+      return;
     }
   }
+  start = 0;
 }
 
 function Init() {
-  Draw();
+  Draw(-2);
 }
 
 Init();
