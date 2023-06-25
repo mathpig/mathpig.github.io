15eef56ad649c745e98ea24cf3de1cef22983a37
mathpig <41877708+mathpig@users.noreply.github.com>
2023-06-24 19:20:48 -0700

    Stuff.

--- battle.js
+++ battle.js
@@ -94,18 +94,19 @@ class Warrior {
   }
 
   tick() {
-    var bestDistance = 1000;
+    var bestDistance = 10000;
     var index = 0;
     for (var i = 0; i < entities.length; ++i) {
-      if (entities[i].team != this.team) {
-        var distance = Math.sqrt(Math.pow(entities[i].x - this.x, 2) + Math.pow(entities[i].y - this.y, 2));
-        if (distance < bestDistance) {
-          bestDistance = distance;
-          index = i;
-        }
+      if ((entities[i] instanceof Bullet) || entities[i].team == this.team) {
+        continue;
+      }
+      var distance = Math.sqrt(Math.pow(entities[i].x - this.x, 2) + Math.pow(entities[i].y - this.y, 2));
+      if (distance < bestDistance) {
+        bestDistance = distance;
+        index = i;
       }
     }
-    if (bestDistance == 1000) {
+    if (bestDistance == 10000) {
       this.vx = 0;
       this.vy = 0;
     }
@@ -115,12 +116,10 @@ class Warrior {
     }
     this.x += this.vx;
     this.y += this.vy;
-    this.x = Math.min(Math.max(this.x, 50), screen.width - 50);
-    this.y = Math.min(Math.max(this.y, 50), screen.height - 50);
     var revert = false;
     var target = -1;
     for (var i = 0; i < entities.length; ++i) {
-      if (this.touches(entities[i]) && this !== entities[i]) {
+      if (this.touches(entities[i]) && entities[i].touches(this) && this !== entities[i]) {
         if (entities[i].team != this.team && this.cooldown <= 0) {
           this.cooldown = this.maxCooldown;
           entities[i].health -= randint(this.minAttack, this.maxAttack);
@@ -226,8 +225,153 @@ class Rogue extends Warrior {
   }
 }
 
+class Bullet {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.vx = 0;
+    this.vy = 0;
+    this.attack = 0;
+    this.size = 10;
+    this.source = 0;
+  }
+
+  setPosition(x, y) {
+    this.x = x;
+    this.y = y;
+    return this;
+  }
+
+  setVelocity(vx, vy) {
+    this.vx = vx;
+    this.vy = vy;
+    return this;
+  }
+
+  setAttack(attack) {
+    this.attack = attack;
+    return this;
+  }
+
+  setSize(size) {
+    this.size = size;
+    return this;
+  }
+
+  setSource(source) {
+    this.source = source;
+    return this;
+  }
+
+  draw() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(this.x - this.size / 2, this.y - this.size / 2, this.size, this.size);
+  }
+
+  drawhealthbar() {
+  }
+
+  touches(other) {
+    return false;
+  }
+
+  remove() {
+    for (var i = 0; i < entities.length; ++i) {
+      if (entities[i] === this) {
+        var index = i;
+        break;
+      }
+    }
+    entities.splice(index, 1);
+  }
+
+  tick() {
+    this.x += this.vx;
+    this.y += this.vy;
+    if (this.x < 100 || this.x > (screen.width - 100) || this.y < 100 || this.y > (screen.height - 100)) {
+      this.remove();
+      return;
+    }
+    var index = -1;
+    for (var i = 0; i < entities.length; ++i) {
+      if (entities[i].touches(this) && entities[i] !== this.source) {
+        index = i;
+        break;
+      }
+    }
+    if (index >= 0) {
+      entities[index].health -= this.attack;
+      if (entities[index].health <= 0) {
+        entities.splice(index, 1);
+      }
+      this.remove();
+    }
+  }
+}
+
+class Archer extends Warrior {
+  constructor() {
+    super();
+    this.health = randint(40, 60);
+    this.maxHealth = this.health;
+    this.minAttack = 32;
+    this.maxAttack = 64;
+    this.size = 15;
+    this.cooldown = 160;
+    this.maxCooldown = this.cooldown;
+    this.bulletSize = 8;
+    this.bulletSpeed = 3;
+  }
+
+  setBulletSize(bulletSize) {
+    this.bulletSize = bulletSize;
+    return this;
+  }
+
+  setBulletSpeed(bulletSpeed) {
+    this.bulletSpeed = bulletSpeed;
+    return this;
+  }
+
+  tick() {
+    var bestDistance = 10000;
+    var index = 0;
+    for (var i = 0; i < entities.length; ++i) {
+      if ((entities[i] instanceof Bullet) || entities[i].team == this.team) {
+        continue;
+      }
+      var distance = Math.sqrt(Math.pow(entities[i].x - this.x, 2) + Math.pow(entities[i].y - this.y, 2));
+      if (distance < bestDistance) {
+        bestDistance = distance;
+        index = i;
+      }
+    }
+    if (bestDistance != 10000 && this.cooldown <= 0) {
+      var bvx = this.bulletSpeed * (entities[index].x - this.x) / bestDistance;
+      var bvy = this.bulletSpeed * (entities[index].y - this.y) / bestDistance;
+      entities.push(new Bullet().setPosition(this.x, this.y).setVelocity(bvx, bvy).setAttack(randint(this.minAttack, this.maxAttack)).setSize(this.bulletSize).setSource(this));
+      this.cooldown = this.maxCooldown;
+    }
+    this.cooldown -= 1;
+  }
+}
+
+class Gunner extends Archer {
+  constructor() {
+    super();
+    this.health = randint(20, 30);
+    this.maxHealth = this.health;
+    this.minAttack = 1;
+    this.maxAttack = 2;
+    this.cooldown = 5;
+    this.maxCooldown = this.cooldown;
+    this.bulletSize = 4;
+    this.bulletSpeed = 5;
+  }
+}
+
 entities.push(new Giant().setPosition(screen.width / 2, screen.height / 2).setTeam(0));
-for (var i = 0; i < 500; ++i) {
+for (var i = 0; i < 250; ++i) {
   while (true) {
     var x = randint(100, screen.width - 100);
     var y = randint(100, screen.height - 100);
@@ -242,8 +386,14 @@ for (var i = 0; i < 500; ++i) {
     else if (val == 2) {
       entities.push(new Decoy().setPosition(x, y).setTeam(team));
     }
-    else if (val <= 4) {
+    else if (val == 3) {
       entities.push(new Rogue().setPosition(x, y).setTeam(team));
+    }
+    else if (val == 4) {
+      entities.push(new Gunner().setPosition(x, y).setTeam(team));
+    }
+    else if (val <= 9) {
+      entities.push(new Archer().setPosition(x, y).setTeam(team));
     }
     else {
       entities.push(new Warrior().setPosition(x, y).setTeam(team));
