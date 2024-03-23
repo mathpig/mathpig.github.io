1104cf33cb7a25db674351c26e0a161efee9208e
mathpig <41877708+mathpig@users.noreply.github.com>
2024-03-23 09:04:21 -0700

    More progres.

--- castles/castles.js
+++ castles/castles.js
@@ -3,35 +3,46 @@
 var screen = document.getElementById("screen");
 var ctx = screen.getContext("2d");
 
-const blockSize = 20;
+const blockSize = 25;
 
 var entities = [];
 
+function intervalTouches(a, b, c, d) {
+  return (b > c && d > a);
+}
+
+function touches(e1, e2) {
+  return (intervalTouches(e1.x - e1.size / 2, e1.x + e1.size / 2, e2.x - e2.size / 2, e2.x + e2.size / 2) &&
+          intervalTouches(e1.y - e1.size / 2, e1.y + e1.size / 2, e2.y - e2.size / 2, e2.y + e2.size / 2));
+}
+
+var keySet = {};
+
 var map = ["",
-           "               BHHB    BHHB    BHHB    BHHB",
-           "        BBBBBHHBHHB    BHHB    BHHB    BHHB",
-           "        BHHHHHHBBBB    BBBB    BBBB    BBBB",
-           "        BHHHHBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
-           "        BHHHBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
-           "        BHHBHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHB",
-           "        BHBHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHB",
-           "        BBHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHB",
-           "        BHBHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHB",
-           "        BHHBHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHB",
-           "        BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBHHBB",
-           "        BHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHBHHB",
-           "        BHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHBHB",
-           "        BHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHBB",
-           "        BHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHBHB",
-           "        BHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHBHHB",
-           "        BBHHBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
-           "        BHHBHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHB",
-           "        BHBHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHB",
-           "        BBHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH",
-           "        BHBHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH",
-           "        BHHBHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH",
-           "GGGGGGGGBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBGGGGWWWWWWWWGGGG",
-           "DDDDDDDDBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBDDDDWWWWWWWWDDDD",
+           "B              BHHB    BHHB    BHHB    BHHB                B",
+           "B       BBBBBHHBHHB    BHHB    BHHB    BHHB                B",
+           "B       BHHHHHBBBBBB  BBBBBB  BBBBBB  BBBBBB               B",
+           "B       BHHHHBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB               B",
+           "B       BHHHBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB               B",
+           "B       BHHBHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHB               B",
+           "B       BHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHB               B",
+           "B       BBHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHB               B",
+           "B       BHHHHHHHHHHHSSHHHHHHHHHHHHHHHHHHHHHB               B",
+           "B       BHHBHHHHHHHHSSHHHHHHHHHHHHHHHHHHHHHB               B",
+           "B       BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBHHHB               B",
+           "B       BHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHBHHB               B",
+           "B       BHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHB               B",
+           "B       BHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHBB               B",
+           "B       BHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHB               B",
+           "B       BHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHBHHB               B",
+           "B       BHHHBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB               B",
+           "B       BHHBHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHB               B",
+           "B       BHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHB               B",
+           "B       BBHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH               B",
+           "B       HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH               B",
+           "B       HHHBHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH               B",
+           "GGGGGGGGBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBGGGGLLLLLLLLGGGG",
+           "DDDDDDDDBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBDDDDLLLLLLLLDDDD",
            "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD",
            "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD"];
 
@@ -56,11 +67,15 @@ function Init() {
       else if (block == "D") {
         entities.push(new Dirt().setPosition(x, y));
       }
-      else if (block == "W") {
-        entities.push(new Water().setPosition(x, y));
+      else if (block == "L") {
+        entities.push(new Lava().setPosition(x, y));
+      }
+      else if (block == "S") {
+        entities.push(new Spawner().setPosition(x, y));
       }
     }
   }
+  entities.push(player);
 }
 
 function distance(e1, e2) {
@@ -72,7 +87,8 @@ class Block {
     this.x = 0;
     this.y = 0;
     this.size = blockSize;
-    this.doCollision = true;
+    this.isCollidable = true;
+    this.color = "black";
   }
 
   setPosition(x, y) {
@@ -91,6 +107,11 @@ class Block {
     return this;
   }
 
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x - this.size / 2 - 1, this.y - this.size / 2 - 1, this.size + 2, this.size + 2);
+  }
+
   tick() {
   }
 }
@@ -106,7 +127,7 @@ class Brick extends Block {
     ctx.fillStyle = this.color1;
     ctx.fillRect(this.x - this.size / 2, this.y - this.size / 2, this.size, this.size);
     ctx.strokeStyle = this.color2;
-    ctx.lineWidth = 2;
+    ctx.lineWidth = (blockSize / 10);
     ctx.beginPath();
     ctx.moveTo(this.x - this.size / 2, this.y - this.size / 2);
     ctx.lineTo(this.x + this.size / 2, this.y - this.size / 2);
@@ -126,38 +147,47 @@ class Brick extends Block {
 class BackgroundBrick extends Brick {
   constructor() {
     super();
-    this.doCollision = false;
+    this.isCollidable = false;
     this.color1 = "rgb(64, 64, 64)";
     this.color2 = "rgb(0, 0, 0)";
   }
 }
 
 class Grass extends Block {
-  draw() {
-    ctx.fillStyle = "green";
-    ctx.fillRect(this.x - this.size / 2, this.y - this.size / 2, this.size, this.size);
+  constructor() {
+    super();
+    this.color = "green";
   }
 }
 
 class Dirt extends Block {
-  draw() {
-    ctx.fillStyle = "brown";
-    ctx.fillRect(this.x - this.size / 2, this.y - this.size / 2, this.size, this.size);
+  constructor() {
+    super();
+    this.color = "brown";
   }
 }
 
-class Water extends Block {
-  draw() {
-    ctx.fillStyle = "blue";
-    ctx.fillRect(this.x - this.size / 2, this.y - this.size / 2, this.size, this.size);
+class Lava extends Block {
+  constructor() {
+    super();
+    this.color = "red";
+  }
+}
+
+class Spawner extends Block {
+  constructor() {
+    super();
+    this.isCollidable = false;
+    this.color = "maroon";
   }
 }
 
 class Knight {
   constructor() {
-    this.speed = 1;
+    this.speed = 2;
     this.x = 0;
     this.y = 0;
+    this.vy = 0;
     this.size = 20;
     this.color = "blue";
   }
@@ -170,6 +200,11 @@ class Knight {
   setPosition(x, y) {
     this.x = x;
     this.y = y;
+    return this;
+  }
+
+  setVelocity(vy) {
+    this.vy = vy;
     return this;
   }
 
@@ -189,6 +224,32 @@ class Knight {
   }
 
   tick() {
+    var oldX = this.x;
+    if (keySet["ArrowLeft"]) {
+      this.x -= this.speed;
+    }
+    if (keySet["ArrowRight"]) {
+      this.x += this.speed;
+    }
+    for (var i = 0; i < entities.length; ++i) {
+      if (entities[i] instanceof Block && entities[i].isCollidable && touches(this, entities[i])) {
+        this.x = oldX;
+        break;
+      }
+    }
+    this.vy += 0.3;
+    this.vy *= 0.95;
+    var oldY = this.y;
+    this.y += this.vy;
+    for (var i = 0; i < entities.length; ++i) {
+      if (entities[i] instanceof Block && entities[i].isCollidable && touches(this, entities[i])) {
+        this.y = oldY;
+        if (this.vy > 0 && keySet["ArrowUp"]) {
+          this.vy -= 12;
+        }
+        break;
+      }
+    }
   }
 }
 
@@ -202,7 +263,9 @@ function Draw() {
   ctx.fillStyle = "cyan";
   ctx.fillRect(0, 0, l1 * blockSize, l2 * blockSize);
   for (var i = 0; i < entities.length; ++i) {
-    entities[i].draw();
+    if (distance(player, entities[i]) < 250) {
+      entities[i].draw();
+    }
   }
 }
 
@@ -213,5 +276,15 @@ function Tick() {
   Draw();
 }
 
+var player = new Knight().setPosition(420, 200);
+
 Init();
 setInterval(Tick, 25);
+
+window.onkeydown = function(e) {
+  keySet[e.key] = true;
+};
+
+window.onkeyup = function(e) {
+  keySet[e.key] = false;
+};
