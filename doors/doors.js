68a550ff25531dae0cced511231166f6a206e02b
mathpig <41877708+mathpig@users.noreply.github.com>
2024-05-12 14:09:52 -0700

    Stuff.

--- doors/doors.js
+++ doors/doors.js
@@ -9,6 +9,19 @@ var entities = [];
 var otherEntities = [];
 var player;
 
+var smilerColorMap = ["       ",
+                      "  W W  ",
+                      "  W W  ",
+                      "       ",
+                      " W   W ",
+                      "  WWW  ",
+                      "       "];
+var smilerColors = {
+  " ": "rgb(0, 0, 0)",
+  "W": "rgb(255, 255, 255)",
+};
+
+var hasLost = false;
 var lightsOut = false;
 
 function inInterval(val, a, b) {
@@ -79,6 +92,8 @@ var map = [
 function Init() {
   entities = [];
   otherEntities = [];
+  hasLost = false;
+  lightsOut = false;
   for (var i = 0; i < map.length; ++i) {
     for (var j = 0; j < map[i].length; ++j) {
       var block = map[i][j];
@@ -104,7 +119,7 @@ function Init() {
       }
       else if (block == "S") {
         entities[val] = new Light().setPosition(x, y);
-        player = new Knight().setPosition(x, y);
+        player = new Player().setPosition(x, y);
       }
     }
   }
@@ -240,13 +255,15 @@ class Light extends Brick {
   }
 }
 
-class Knight {
+class Player {
   constructor() {
     this.speed = (blockSize / 5);
     this.x = 0;
     this.y = 0;
     this.size = blockSize * 0.8;
     this.color = "blue";
+    this.isEnemy = false;
+    this.hasBumped = false;
   }
 
   setSpeed(speed) {
@@ -270,75 +287,145 @@ class Knight {
     return this;
   }
 
+  setIsEnemy(isEnemy) {
+    this.isEnemy = isEnemy;
+    return this;
+  }
+
   draw() {
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x - this.size / 2, this.y - this.size / 2, this.size, this.size);
   }
 
-  tick() {
-    var val = 0;
+  getDir() {
+    var vx = 0;
     if (keySet["ArrowLeft"]) {
-      val--;
+      vx--;
     }
     if (keySet["ArrowRight"]) {
-      val++;
+      vx++;
     }
-    for (var i = 0; i < this.speed; ++i) {
-      this.x += val;
-      var failed = false;
-      for (var j in entities) {
-        if (touches(this, entities[j]) && entities[j].isCollidable) {
-          failed = true;
-          break;
-        }
-      }
-      if (failed) {
-        this.x -= val;
-        break;
-      }
-      var failed = false;
-      for (var j = 0; j < otherEntities.length; ++j) {
-        if (otherEntities[j] !== this && touches(this, otherEntities[j])) {
-          failed = true;
-          break;
-        }
-      }
-      if (failed) {
-        this.x -= val;
-        break;
-      }
-    }
-    var val = 0;
+    var vy = 0;
     if (keySet["ArrowUp"]) {
-      val--;
+      vy--;
     }
     if (keySet["ArrowDown"]) {
-      val++;
+      vy++;
     }
+    return [vx, vy];
+  }
+
+  tick() {
+    var val = this.getDir();
+    this.hasBumped = false;
+    var val1 = val[0];
+    var val2 = val[1];
     for (var i = 0; i < this.speed; ++i) {
-      this.y += val;
+      this.x += val1;
       var failed = false;
-      for (var j in entities) {
-        if (touches(this, entities[j]) && entities[j].isCollidable) {
-          failed = true;
-          break;
+      for (var x = -1; x <= 1; ++x) {
+        for (var y = -1; y <= 1; ++y) {
+          var e = entities[findBlock(this.x + blockSize * x, this.y + blockSize * y)];
+          if (touches(this, e) && e.isCollidable) {
+            failed = true;
+            break;
+          }
         }
       }
       if (failed) {
-        this.y -= val;
+        this.hasBumped = true;
+        this.x -= val1;
         break;
       }
-      var failed = false;
+      failed = false;
       for (var j = 0; j < otherEntities.length; ++j) {
         if (otherEntities[j] !== this && touches(this, otherEntities[j])) {
+          if (!otherEntities[j].isEnemy) {
+            hasLost = true;
+          }
           failed = true;
           break;
         }
       }
       if (failed) {
-        this.y -= val;
+        this.hasBumped = true;
+        this.x -= val1;
         break;
       }
+    }
+    for (var i = 0; i < this.speed; ++i) {
+      this.y += val2;
+      var failed = false;
+      for (var x = -1; x <= 1; ++x) {
+        for (var y = -1; y <= 1; ++y) {
+          var e = entities[findBlock(this.x + blockSize * x, this.y + blockSize * y)];
+          if (touches(this, e) && e.isCollidable) {
+            failed = true;
+            break;
+          }
+        }
+      }
+      if (failed) {
+        this.hasBumped = true;
+        this.y -= val2;
+        break;
+      }
+      failed = false;
+      for (var j = 0; j < otherEntities.length; ++j) {
+        if (otherEntities[j] !== this && touches(this, otherEntities[j])) {
+          if (!otherEntities[j].isEnemy) {
+            hasLost = true;
+          }
+          failed = true;
+          break;
+        }
+      }
+      if (failed) {
+        this.hasBumped = true;
+        this.y -= val2;
+        break;
+      }
+    }
+  }
+}
+
+class Smiler extends Player {
+  constructor() {
+    super();
+    this.speed = (blockSize / 10);
+    this.isEnemy = true;
+    this.direction = Math.floor(Math.random() * 4);
+    this.colorMap = smilerColorMap;
+    this.colors = smilerColors;
+  }
+
+  draw() {
+    for (var i = 0; i < this.colorMap.length; ++i) {
+      for (var j = 0; j < this.colorMap[0].length; ++j) {
+        ctx.fillStyle = this.colors[this.colorMap[i][j]];
+        ctx.fillRect(this.x - this.size / 2 + j * this.size / this.colorMap[0].length - 1,
+                     this.y - this.size / 2 + i * this.size / this.colorMap.length - 1,
+                     this.size / this.colorMap[0].length + 2,
+                     this.size / this.colorMap.length + 2);
+      }
+    }
+  }
+
+  getDir() {
+    if (this.hasBumped || Math.random() < 0.01) {
+      this.direction = (this.direction + 2 * Math.round(Math.random()) + 1) % 4;
+    }
+    if (this.direction == 0) {
+      return [0, -1];
+    }
+    else if (this.direction == 1) {
+      return [1, 0];
+    }
+    else if (this.direction == 2) {
+      return [0, 1];
+    }
+    else {
+      return [-1, 0];
     }
   }
 }
@@ -347,6 +434,7 @@ function doCasts(maxDist) {
   var rays = 1800;
   var increment = 10;
   var toDraw = new Set();
+  var toDraw2 = [];
   for (var angle = 0; angle < rays; ++angle) {
     var ang = (angle * 2 * Math.PI / rays);
     var x = player.x;
@@ -359,6 +447,11 @@ function doCasts(maxDist) {
         continue;
       }
       toDraw.add(e);
+      for (var i = 0; i < otherEntities.length; ++i) {
+        if (isInside(x, y, otherEntities[i])) {
+          toDraw2.push(otherEntities[i]);
+        }
+      }
       if (entities[e].isCollidable) {
         break;
       }
@@ -367,26 +460,54 @@ function doCasts(maxDist) {
   for (var i of toDraw) {
     entities[i].draw();
   }
+  for (var i = 0; i < toDraw2.length; ++i) {
+    toDraw2[i].draw();
+  }
 }
 
-screen.width = 20 * blockSize;
-screen.height = 10 * blockSize;
-
 function Draw() {
+  screen.width = window.innerWidth;
+  screen.height = window.innerHeight;
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, screen.width, screen.height);
+  if (hasLost) {
+    for (var i = 0; i < smilerColorMap.length; ++i) {
+      for (var j = 0; j < smilerColorMap[0].length; ++j) {
+        ctx.fillStyle = smilerColors[smilerColorMap[i][j]];
+        ctx.fillRect(screen.width / 2 - screen.height / 2 + j * screen.height / smilerColorMap[0].length - 1,
+                     i * screen.height / smilerColorMap.length - 1,
+                     screen.height / smilerColorMap[0].length + 2,
+                     screen.height / smilerColorMap.length + 2);
+      }
+    }
+    return;
+  }
   ctx.save();
   ctx.translate(screen.width / 2 + player.size / 2 - player.x, screen.height / 2 + player.size / 2 - player.y);
   doCasts(12 * blockSize);
-  for (var i = 0; i < otherEntities.length; ++i) {
-    otherEntities[i].draw();
-  }
   ctx.restore();
 }
 
+var time = 0;
+
 function Tick() {
-  if (Math.random() < 0.0025) {
+  time++;
+  if (hasLost) {
+    Draw();
+    return;
+  }
+  if (Math.random() < 0.0025 && time >= 400) {
     lightsOut = !lightsOut;
+    if (lightsOut) {
+      for (i in entities) {
+        if (entities[i] instanceof Light) {
+          otherEntities.push(new Smiler().setPosition(entities[i].x, entities[i].y));
+        }
+      }
+    }
+    else {
+      otherEntities = [player];
+    }
   }
   for (var i = 0; i < otherEntities.length; ++i) {
     otherEntities[i].tick();
